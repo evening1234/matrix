07c3c5ea3623a837576d1469aec5ca26dd566328
bartek.wieczorek <bartek.wieczorek04@gmail.com>
2024-01-04 21:19:29 +0100

    hot fixes before demo

--- main.cpp
+++ main.cpp
@@ -2,112 +2,107 @@
 #include <cstdio>
 #include <ctime>
 #include <unistd.h>
-using namespace std;
-int **reading(const char *fileName) {
-    int c,r;
+
+int **readMatrix(const char *fileName) {
+    int c, r;
     int **matrix = (int **) malloc(sizeof(int *) * r);
     FILE *file = fopen(fileName, "r");
-    fscanf(file, "%d\n",&r);
-    fscanf(file, "%d\n",&c);
+    fscanf(file, "%d %d", &r, &c);
     for (int i = 0; i < r; i++) {
-        matrix[i] = (int *) malloc(sizeof(int *) * c);
+        matrix[i] = (int *) malloc(sizeof(int) * c);
         for (int j = 0; j < c; j++) {
             fscanf(file, "%d ", &(matrix[i][j]));
         }
-        cout << endl;
     }
     fclose(file);
+    printf("\n");
     return matrix;
 }
+
 void saveMatrix(int r, int c, int **matrix, const char *fileName) {
     FILE *file = fopen(fileName, "w");
     fprintf(file, "%d %d\n", r, c);
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < c; j++) {
-            cout << matrix[i][j] << " ";
+            printf("%d ", matrix[i][j]);
             fprintf(file, "%d ", matrix[i][j]);
         }
-        cout << endl;
+        printf("\n");
         fprintf(file, "\n");
     }
+    printf("\n");
 
     fclose(file);
 }
+
 int **generateMatrix(int r, int c) {
-    int **arr = (int **) malloc(sizeof(int *) * r);
+    int **matrix = (int **) malloc(sizeof(int *) * r);
     for (int i = 0; i < r; i++) {
-        arr[i] = (int *) malloc(sizeof(int) * c);
+        matrix[i] = (int *) malloc(sizeof(int) * c);
         for (int j = 0; j < c; j++) {
-            arr[i][j] = rand() % 100;
+            matrix[i][j] = rand() % 100;
         }
     }
-    return arr;
+    return matrix;
 }
-int **last(int **arr1,int **arr2,int c, int r) {
+
+int **multiply(int **matrix1, int **matrix2, int r, int c) {
     int **result = (int **) malloc(sizeof(int *) * r);
     for (int i = 0; i < r; i++) {
         result[i] = (int *) malloc(sizeof(int) * c);
-        cout<<endl;
         for (int j = 0; j < c; j++) {
             int sum = 0;
-            for(int k=0;k<c;k++) {
-                sum += arr1[i][k] * arr2[k][j];
-                //printf("sum = %d\n", sum);
+            for (int k = 0; k < r; k++) {
+                sum += matrix1[i][k] * matrix2[k][j];
             }
             result[i][j] = sum;
-            //printf("% d", result[i][j]);
         }
     }
     return result;
 }
+
+void freeMatrix(int **matrix, int c) {
+    for (int i = 0; i < c; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
 int main() {
-    int n;
-    int m;
-    int x;
-    int y;
-    printf("podaj ilość wierszy pierwszej macierzy:");
-    scanf("%d",&n);
-    printf("podaj ilość kolumn pierwszej macierzy:");
-    scanf("%d",&m);
+    int r1, c1, r2, c2;
 
-    printf("podaj ilość wierszy drugiej macierzy:");
-    scanf("%d",&x);
-    printf("podaj ilość kolumn drugiej macierzy:");
-    scanf("%d",&y);
-    cout<<endl;
-
+    printf("podaj ilość wierszy pierwszej macierzy: ");
+    scanf("%d", &r1);
+    printf("podaj ilość kolumn pierwszej macierzy: ");
+    scanf("%d", &c1);
+    printf("podaj ilość wierszy drugiej macierzy: ");
+    scanf("%d", &r2);
+    printf("podaj ilość kolumn drugiej macierzy: ");
+    scanf("%d", &c2);
+    printf("\n");
 
     srand(time(NULL));
-    if (m != x) {
-        printf("nie mozna mnozyc");
+
+    if (c1 != r2) {
+        printf("nie można mnożyć");
     } else {
-        int **matrix1 = generateMatrix(n, m);
-        int **matrix2 = generateMatrix(x, y);
-        saveMatrix(n, m, matrix1, "matrix1.txt");
-        cout<<endl;
-        saveMatrix(x, y, matrix2, "matrix2.txt");
-        free(matrix1);
-        free(matrix2); // TODO free memory correctly, that is, remove free columns/rows
+        int **matrix1 = generateMatrix(r1, c1);
+        int **matrix2 = generateMatrix(r2, c2);
+        saveMatrix(r1, c1, matrix1, "matrix1.txt");
+        saveMatrix(r2, c2, matrix2, "matrix2.txt");
+//        freeMatrix(matrix1, c1);
+//        freeMatrix(matrix2, c2);
+
+        matrix1 = readMatrix("matrix1.txt");
+        matrix2 = readMatrix("matrix2.txt");
+
+        int **matrix = multiply(matrix1, matrix2, r1, c2);
+        saveMatrix(r1, c2, matrix, "result.txt");
+
+        freeMatrix(matrix, c2);
+        freeMatrix(matrix1, c1);
+        freeMatrix(matrix2, c2);
     }
-
-
-    int z = m *n;
-    int e = x * y;
-
-
-    int **arr1 = reading("matrix1.txt");
-    cout<<endl;
-    int **arr2 = reading("matrix2.txt");
-    cout<<endl;
-    // TODO free result, free intermediete matrixes
-
-
-    int **matrix = last(arr1,arr2,y,n);
-    FILE *file = fopen("last.txt", "w");
-    fclose(file);
-    saveMatrix(n,x,matrix,"last.txt");
-
-
 
     return 0;
 }
